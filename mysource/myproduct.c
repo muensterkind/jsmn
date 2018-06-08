35c10f8d86acb0c45a9eafc046ad3dbe889e99ef
u21700648 <21700648@handong.edu>
2018-06-08 17:34:19 +0900

    add printProduct initially and try to execute, but segmentation fault

--- mysource/myproduct.c
+++ mysource/myproduct.c
@@ -37,25 +37,21 @@ char *readJSONFile() {
 	return STRING;
 }
 
-/* store JSON_STRING's tokens in product structures */
-int makeProduct(const char *json, jsmntok_t *t, int tokcount, product_t * p[]) {
-	int i, count = 0;
-
-	for(i = 1; i < tokcount; i++) {
-		if((t+i)->type == JSMN_OBJECT && (t+i)->parent->type == JSMN_ARRAY) { // if the token means product object
-			p[count] = (product_t *)malloc(sizeof(product_t));
-			assert(p[count] != NULL);
-			inputInfo(json, t, i + 1, p[count]);
-			count++;
-		}
-	}
-	return count;
+/* check the company name */
+company_t checkCompany(char *json, jsmntok_t *t, int num) {
+	company_t value = 0;
+	if (jsoneq(json, &t[num], "탐사") == 0) value = TAMSAA;
+	else if (jsoneq(json, &t[num], "농심") == 0) value = NONGSHIM;
+	else if (jsoneq(json, &t[num], "다우니") == 0) value = DOWNY;
+	else if (jsoneq(json, &t[num], "센스") == 0) value = SENSE;
+	else value = -1;
+	return value;
 }
 
 /* input information into the productList */
 void inputInfo(char *json, jsmntok_t *t, int tokNum, product_t *product) {
 	while((t+tokNum)->type != JSMN_OBJECT) {
-		if (jsoneq(json, &t[tokNum], "company") == 0) product->company = checkCompany(tokNum+1);
+		if (jsoneq(json, &t[tokNum], "company") == 0) product->company = checkCompany(json, t, tokNum+1);
 		else if (jsoneq(json, &t[tokNum], "name") == 0)
 			strncpy(product->name, json + (t+tokNum+1)->start, (t+tokNum+1)->end - (t+tokNum+1)->start);
 		else if (jsoneq(json, &t[tokNum], "price") == 0) product->price = stringToInt(json, t, tokNum+1);
@@ -64,43 +60,19 @@ void inputInfo(char *json, jsmntok_t *t, int tokNum, product_t *product) {
 	}
 }
 
-/* check the company name */
-company_t checkCompany(int num) {
-	company_t value = 0;
-	if (jsoneq(JSON_STRING, &t[num], "탐사") == 0) value = TAMSAA;
-	else if (jsoneq(JSON_STRING, &t[num], "농심") == 0) value = NONGSHIM;
-	else if (jsoneq(JSON_STRING, &t[num], "다우니") == 0) value = DOWNY;
-	else if (jsoneq(JSON_STRING, &t[num], "센스") == 0) value = SENSE;
-	else value = -1;
-	return value;
-}
+/* store JSON_STRING's tokens in product structures */
+int makeProduct(char *json, jsmntok_t *t, int tokcount, product_t * p[]) {
+	int i, count = 0;
 
-/* print objects list */
-void printList(char *jsonstr, jsmntok_t *t, NameTokenInfo *nameTokenInfo) {
-	printf("************************************************\n");
-	printf("번호    제품명  제조사  가격    개수    총가격  \n");
-	int objectNo = 0;
-	int company = 0, name = 0, price = 0, count = 0;
-	int priceValue = 0, countValue = 0;
-
-	while(nameTokenInfo[objectNo + 1].objectindex != '\0') {
-		name = giveTokindex(jsonstr, t, nameTokenInfo, objectNo, "name"); // invoke name's tokindex
-		company = giveTokindex(jsonstr, t, nameTokenInfo, objectNo, "company"); // invoke company's tokindex
-		price = giveTokindex(jsonstr, t, nameTokenInfo, objectNo, "price"); // invoke price's tokindex
-		priceValue = stringToInt(jsonstr, t, price); // change the string into int value
-		count = giveTokindex(jsonstr, t, nameTokenInfo, objectNo, "count"); // invoke count's tokindex
-		countValue = stringToInt(jsonstr, t, count); // change the string into int value
-
-		printf("%-8d", objectNo + 1);
-		printf("%-11.*s", (t+name)->end - (t+name)->start, jsonstr + (t+name)->start);
-		printf("%-10.*s", (t+company)->end - (t+company)->start, jsonstr + (t+company)->start);
-		printf("%-8d", priceValue);
-		printf("%-8d", countValue);
-		printf("%-8d\n", priceValue * countValue);
-
-		objectNo++;
+	for(i = 1; i < tokcount; i++) {
+		if((t+i)->type == JSMN_OBJECT && (t+(t+i)->parent)->type == JSMN_ARRAY) { // if the token means product object
+			p[count] = (product_t *)malloc(sizeof(product_t));
+			assert(p[count] != NULL);
+			inputInfo(json, t, i + 1, p[count]);
+			count++;
+		}
 	}
-	printf("************************************************\n");
+	return count;
 }
 
 /* change token string that means integer into int type */
@@ -111,6 +83,20 @@ int stringToInt(char *jsonstr, jsmntok_t *t, int num) {
 	int value = atoi(STRING);
 	free(STRING);
 	return value;
+}
+
+void printProduct(product_t * p[], int pcount) {
+	int i = 0;
+	printf("****************************************\n");
+	printf("번호    제품명  제조사  가격    개수    \n");
+	for(i = 0; i < pcount; i++) {
+		printf("%-8d", i + 1);
+		printf("%-10s", p[i]->name);
+		printf("%-10c", p[i]->company);
+		printf("%-8d", p[i]->price);
+		printf("%-8d\n", p[i]->count);
+	}
+	printf("****************************************\n");
 }
 
 static char *JSON_STRING;
@@ -124,26 +110,24 @@ int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 }
 
 int main() {
-	int i = 0;
 	int r = 0;
-	int pcount = 0;
-	jsmn_parser p;
+	int pcount = 0; // for counting the number of products
+	jsmn_parser p; // parser
 	jsmntok_t t[128]; /* We expect no more than 128 tokens */
 	product_t *coupangList[20]; // for saving product information
 
-	JSON_STRING = readJSONFile();
-	if(JSON_STRING == "-1") return EXIT_SUCCESS;
+	JSON_STRING = readJSONFile(); // read input file as a string
+	if(JSON_STRING == "-1") return EXIT_SUCCESS; // if failing to read the file, exit
 
-	jsmn_init(&p);
-	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
+	jsmn_init(&p); // initialize the parser
+	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0])); // parse the string into tokens and store the number of tokens
 	if (r < 0) {
 		printf("Failed to parse JSON: %d\n", r);
 		return 1;
 	}
 
-	pcount = makeProduct(JSON_STRING, t, r, coupangList);
-
-	printList(JSON_STRING, t, Index);
+	pcount = makeProduct(JSON_STRING, t, r, coupangList); // store the product information and save the number of products
+	printProduct(coupangList, pcount); // print products
 
 	return EXIT_SUCCESS;
 }
